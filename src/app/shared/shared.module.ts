4d65d0545d8d91e7fa61ed85a4aad49d99a86f1c
mthu <1652599@hcmut.edu.vn>
2019-10-19 15:24:06 +0700

    content detail component added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,15 +4,19 @@ import { LogoComponent } from './logo/logo.component';
 import { CourseComponent } from './course/course.component';
 import { CourseContentComponent } from './course-content/course-content.component';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { CourseContentDetailComponent } from './course-content-detail/course-content-detail.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     LogoComponent,
     CourseComponent,
     CourseContentComponent,
+    CourseContentDetailComponent,
   ],
   imports: [
     RouterModule,
+    CommonModule,
     MatExpansionModule,
   ],
   providers: [],
@@ -21,6 +25,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
     LogoComponent,
     CourseComponent,
     CourseContentComponent,
+    CourseContentDetailComponent,
   ],
 })
 export class SharedModule {
