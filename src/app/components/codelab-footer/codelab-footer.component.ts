e75c0b9443b6b334bd0cb5a8b06805842d7f429a
Toan <kimtoan1998@gmail.com>
2019-10-25 22:13:42 +0700

    [Update] Finish the UI for footer
    Functions are not implemented yet, no interactions with other components

--- src/app/components/codelab-footer/codelab-footer.component.ts
+++ src/app/components/codelab-footer/codelab-footer.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class ProjectErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-codelab-footer',
@@ -7,6 +17,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CodelabFooterComponent implements OnInit {
   
+  projectFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+  matcher = new ProjectErrorStateMatcher();
+
   mode: any;
   modes = [
     {
@@ -20,7 +35,9 @@ export class CodelabFooterComponent implements OnInit {
   ];
   isPythonMode = false;
 
-  
+  isSidebarOpen = false;
+  sidebarIcon = "keyboard_arrow_left";
+
   constructor() { }
 
   ngOnInit() {
@@ -32,4 +49,16 @@ export class CodelabFooterComponent implements OnInit {
     this.mode = this.modes[this.isPythonMode ? 1 : 0];
   }
 
+  openSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+    this.sidebarIcon = this.isSidebarOpen ? "keyboard_arrow_right" : "keyboard_arrow_left";
+  }
+
+  saveProject() {}
+  
+  newProject() {}
+
+  openProject() {}
+
+  downloadProject() {}
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -10,8 +10,12 @@ import { CourseComponent } from "./course/course.component";
 import { CourseContentComponent } from "./course-content/course-content.component";
 import { CourseContentDetailComponent } from "./course-content-detail/course-content-detail.component";
 import { LogoComponent } from "./logo/logo.component";
+
+// Angular Materail Imports
 import { MatExpansionModule } from "@angular/material/expansion";
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { WordpressComponent } from "./wordpress/wordpress.component";
 import { MainPipeModule } from "../pipes/main-pipe.module";
@@ -40,7 +44,10 @@ import { CodelabFooterComponent } from './codelab-footer/codelab-footer.componen
     MatExpansionModule,
     MainPipeModule,
     NgxBlocklyModule,
-    MatInputModule
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   entryComponents: [],
