77d89a587cb4cb15268a238381c33c8632ee665e
mthu <1652599@hcmut.edu.vn>
2019-11-05 18:14:43 +0700

    authen login (not tested)

--- src/app/components/login-modal/login-modal.component.ts
+++ src/app/components/login-modal/login-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { UserService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-login-modal',
@@ -11,29 +12,46 @@ export class LoginModalComponent implements OnInit {
   form: FormGroup;
   title: string;
   description: string;
+  email;
+  pw;
+  toastr = false;
 
   constructor(
-      private fb: FormBuilder,
-      private dialogRef: MatDialogRef<LoginModalComponent>,
-      @Inject(MAT_DIALOG_DATA) data) {
+    private userService: UserService,
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<LoginModalComponent>,
+    @Inject(MAT_DIALOG_DATA) data) {
 
-      this.title = data.title;
-      this.description = data.description;
+    this.title = data.title;
+    this.description = data.description;
   }
 
   ngOnInit() {
       this.form = this.fb.group({
           title: this.title,
-          description: [this.description, []]
+          description: [this.description, []],
+          email: new FormControl(null),
+          password: new FormControl(null)
       });
   }
 
   save() {
-      this.dialogRef.close(this.form.value);
+    this.onSubmit();
+    this.dialogRef.close(this.form.value);
   }
 
   close() {
       this.dialogRef.close();
+  }
+
+  onSubmit() {
+    this.email = this.form.controls.email.value;
+    this.pw = this.form.controls.password.value;
+    this.userService.attemptAuth('login', { login: this.email, password: this.pw }).subscribe(res => {
+      if (res.success === false) {
+        this.toastr = true;
+      }
+    });
   }
 
 }
